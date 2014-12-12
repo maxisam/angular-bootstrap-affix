9d1cd5cc79eb13df00e8a03852c3dd4e0c82a7ea
Patryk Hes <patryk.hes@laboratorium.ee>
2014-12-12 16:27:37 +0100

    Updated dist files

--- dist/angular-bootstrap-affix.js
+++ dist/angular-bootstrap-affix.js
@@ -1,10 +1,8 @@
 'use strict';
 angular.module('mgcrea.bootstrap.affix', ['mgcrea.jquery']).directive('bsAffix', [
   '$window',
-  '$location',
-  '$routeParams',
   'dimensions',
-  function ($window, $location, $routeParams, dimensions) {
+  function ($window, dimensions) {
     var checkPosition = function (instance, el, options) {
       var scrollTop = window.pageYOffset;
       var scrollHeight = document.body.scrollHeight;
@@ -29,16 +27,27 @@ angular.module('mgcrea.bootstrap.affix', ['mgcrea.jquery']).directive('bsAffix',
       instance.unpin = affix === 'bottom' ? position.top - scrollTop : null;
       el.removeClass(reset).addClass('affix' + (affix ? '-' + affix : ''));
     };
+    var checkCallbacks = function (scope, instance, iElement, iAttrs) {
+      if (instance.affixed) {
+        if (iAttrs.onUnaffix)
+          eval('scope.' + iAttrs.onUnaffix);
+      } else {
+        if (iAttrs.onAffix)
+          eval('scope.' + iAttrs.onAffix);
+      }
+    };
     return {
       restrict: 'EAC',
       link: function postLink(scope, iElement, iAttrs) {
         var instance = { unpin: null };
         angular.element($window).bind('scroll', function () {
           checkPosition(instance, iElement, iAttrs);
+          checkCallbacks(scope, instance, iElement, iAttrs);
         });
         angular.element($window).bind('click', function () {
           setTimeout(function () {
             checkPosition(instance, iElement, iAttrs);
+            checkCallbacks(scope, instance, iElement, iAttrs);
           }, 1);
         });
       }
